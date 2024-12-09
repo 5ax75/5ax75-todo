24b26330270039069a60c2bea09739d92caa6c49
Florian Donckers <fdonckersf@gmail.com>
2024-12-09 13:28:28 +0100

    Installed svgr webpack for svg easy use as react components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/navbar";
+
+const dashboard = () => {
+  return <Navbar />;
+};
+
+export default dashboard;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "@/ressources/svgs/index";
+
+export default function Navbar() {
+  return (
+    <div className="h-20 w-full border-b-accent border-b-4">
+      <Logo />
+    </div>
+  );
+}
