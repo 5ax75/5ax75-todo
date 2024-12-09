691b3812692d652ff3f5162faa077f8a403803f7
Florian Donckers <fdonckersf@gmail.com>
2024-12-09 14:04:07 +0100

    Navbar WIP

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,28 @@
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
 
-export default function Button() {
-    return {
-        
-    }
+interface ButtonProps {
+  text: string;
+  link?: Url;
+  background?: boolean;
+}
+
+export default function Button({ text, link, background = true }: ButtonProps) {
+  return link ? (
+    <Link href={link}>
+      <button
+        className=" rounded-md py-1 px-2"
+        style={{ background: background ? "#1C5E21" : "transparent" }}
+      >
+        {text}
+      </button>
+    </Link>
+  ) : (
+    <button
+      className=" rounded-md py-1 px-2"
+      style={{ background: background ? "#1C5E21" : "transparent" }}
+    >
+      {text}
+    </button>
+  );
 }
